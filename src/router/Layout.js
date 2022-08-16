9c1674974dfe9a1026a11d787f03d2e439fc476f
burakozzdemir <burakozdemir791@gmail.com>
2022-08-16 03:29:44 +0300

    Complete header component and ui

--- src/router/Layout.js
+++ src/router/Layout.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from  "../pages/Home"
+import TvSeries from  "../pages/TvSeries"
+import Movies from  "../pages/Movies"
+import Categories from  "../pages/Categories"
+import Login from  "../pages/Login"
 
 const Layout = () => {
   return (
-    <div>
-        Layout
-    </div>
+    <>
+     <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="tvseries" element={<TvSeries />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </>
   )
 }
 
